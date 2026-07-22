83c13e22b138f72552a7d7949bf2878a0e27536b
SherinShashika <sherinfernando02@gmail.com>
2026-07-22 04:30:59 +0300

    Remove env file and add gitignore

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
+const express = require('express');
 
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Blog API is running');
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`API server listening on port ${port}`);
+})
