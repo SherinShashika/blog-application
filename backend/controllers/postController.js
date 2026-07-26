76f844e36b07827792f7357a6bbbce8a374e2ba3
SherinShashika <sherinfernando02@gmail.com>
2026-07-26 04:20:32 +0300

    Implement update Post API

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -59,8 +59,43 @@ const getPostById = async (req, res) => {
     }
 }
 
+const updatePost = async (req, res) => {
+    try {
+        const { title, content } = req.body;
+
+        const post = await Post.findById(req.params.id)
+
+        if (!post) {
+            return res.status(404).json({
+                message: "Post not found"
+            });
+        }
+
+        if (post.author.toString() !== req.user._id.toString()) {
+            return res.status(403).json({
+                message: "You are not authorized to update this post"
+            });
+        }
+
+        post.title = title || post.title;
+        post.content = content || post.content;
+
+        await post.save();
+
+        res.status(200).json({
+            message: "Post updated successfully",
+            post
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+}
+
 module.exports = {
     createPost,
     getPosts,
     getPostById,
+    updatePost,
 }
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const passport = require('passport');
 
-const { createPost, getPosts, getPostById } = require('../controllers/postController');
+const { createPost, getPosts, getPostById, updatePost } = require('../controllers/postController');
 
 const router = express.Router();
 
@@ -13,5 +13,6 @@ router.get("/", getPosts);
 
 router.get("/:id", getPostById);
 
+router.put("/:id", passport.authenticate('jwt', { session: false }), updatePost);
 
 module.exports = router;
